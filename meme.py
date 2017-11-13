bf44659db52d3fe8d4fdee53c33f33e4b4e28ad3
mhanelia <murilo.aippe@hotmail.com>
2017-11-13 16:32:17 -0200

    update

--- meme.py
+++ meme.py
@@ -1,16 +1,17 @@
-from flask import Flask, request, redirect, url_for , send_from_directory, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 from werkzeug import secure_filename
 import os
 from db import table_meme
 from flask_login import LoginManager
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
-UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static')
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/imgs')
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask('meme')
-i =0
-lm = LoginManager(app)
+app.secret_key = 'some_secret'
+
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
@@ -30,12 +31,12 @@ def upload():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             dados_dict = request.form.to_dict()
             dados_dict['path'] = 'static/imgs/' + filename
-
+            dados_dict['like'] = 0
+            dados_dict['deslike'] = 0
             table_meme.insert(dados_dict)
 
-
             file.save(path)
-            return render_template('index.html', memes = table_meme)
+            return redirect(url_for('index'))
 
 @app.route('/cadastro')
 def cadastro():
@@ -56,5 +57,12 @@ def deslike(id):
         deslikes = table_meme.find_one(id=id)
         data = dict(id=deslikes["id"] ,deslike=deslikes['deslike'] + 1)
         table_meme.update(data, ['id'])
+
+        return redirect(url_for('index'))
+
+@app.route('/apagar/<id>', methods=['GET'])
+def delete(id):
+    if request.method == 'GET':
+        table_meme.delete(id=id)
 
         return redirect(url_for('index'))
